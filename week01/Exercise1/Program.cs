2744f20ea93cd6ea598f47d8ac66b9538fde145f
Jonah Quarshie <jonahquarshie@gmail.com>
2025-03-10 20:40:50 -0800

    added a comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Collections.Immutable;
 
 class Program
+// first project complete.
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name?. ");
+        string first = Console.ReadLine();
+        
+        Console.Write("what is your last name? ");
+         string last = Console.ReadLine();
+         
+         Console.WriteLine($"your name is {last}, {first},{last}");
     }
 }
