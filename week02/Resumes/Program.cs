205042226008f7685a751ed55a5589fefd55834a
Jonah Quarshie <jonahquarshie@gmail.com>
2025-08-03 15:57:53 -0800

    change commit

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,25 +1,28 @@
-
 using System;
 
-class Program
+class Program 
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
-    }
-        public string _name;
-    
-     public List<Job> _jobs = new List<Job>();
-     public void Display()
-    
-    {
-         Console.WriteLine($"Name:{_name}");
-        Console.WriteLine("Jobs:");
-         foreach (Job job in _jobs)
-         {
+        Job job1 = new Job();
+        job1._jobTitle = "Robotics Instructor";
+        job1._company = "";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
-            job.Display();
-        }
+        Job job2 = new Job();
+        job2._jobTitle = "Technical Officer";
+        job2._company = "Ghana Meteorological Agency";
+        job2._startYear = 2011;
+        job2._endYear = 2012;
+
+        Resume myResume = new myResume();
+        Resume name = "Jonah Quarshie";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
-} 
-    
+}
+
