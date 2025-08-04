a464d6dd218685892bb2cf8f8672a36b303bebcc
Jonah Quarshie <jonahquarshie@gmail.com>
2025-08-03 21:49:09 -0800

    Q2 commit

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.IO;
 
-class Program
+public class GoalsTracker
 {
-    static void Main(string[] args)
+    private List<GoalsTracker> _goals = new List<GoalsTracker>();
+
+    private int _accumulatedPoints = 0;
+
+    public int GetAccumulatedPoints()
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+
+        int points = _accumulatedPoints;
+        return points;
+
+    }
+    public void SaveGoals()
+    {
+        string fileName = "";
+        Console.Write("What is the filename? ");
+        fileName = Console.ReadLine();
+
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            int totalAGP = GetAccumulatedPoints();
+            outputFile.WriteLine(totalAGP.ToString());
+
+            foreach (GoalsTracker goal in _goals)
+            {
+                outputFile.WriteLine(goal.Goals());
+            }
+        }
     }
 }
