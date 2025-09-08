e00327d8719790d1c0de9f18bcaa48a9d77e55e3
Jonah Quarshie <jonahquarshie@gmail.com>
2025-09-07 20:33:25 -0800

    week2 pr5

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,28 +1,30 @@
-using System;
 
-class Program 
+namespace Week02.Resumes
 {
-    static void Main(string[] args)
+    public class Program
     {
-        Job job1 = new Job();
-        job1._jobTitle = "Robotics Instructor";
-        job1._company = "";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        static void Main(string[] args)
+        {
+            Job job1 = new Job();
+            job1._jobTitle = "Software Developer";
+            job1._company = "Tech Corp";
+            job1._startYear = 2020;
+            job1._endYear = 2023;
 
-        Job job2 = new Job();
-        job2._jobTitle = "Technical Officer";
-        job2._company = "Ghana Meteorological Agency";
-        job2._startYear = 2011;
-        job2._endYear = 2012;
-
-        Resume myResume = new myResume();
-        Resume name = "Jonah Quarshie";
-
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
-
-        myResume.Display();
+            job1.Display();
+        }
     }
-}
 
+    public class Job
+    {
+        public string _jobTitle;
+        public string _company;
+        public int _startYear;
+        public int _endYear;
+
+        public void Display()
+        {
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+        }
+    }
+} 
