f9a4471eafdd56291205649bb9aabe61c38cf964
Jonah Quarshie <jonahquarshie@gmail.com>
2025-09-07 20:32:13 -0800

    wk2, project4

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -1,15 +1,20 @@
 using System;
+using System.ComponentModel.Design;
 
 public class Job
 
 {
-    public string _jobTitle;
-    public string _company;
-    public int _startYear;
-    public int _endYear;
+    public string _jobTitle = "software developer";
+    public string _company = "tech corp";
+    public int _startYear = 2020;
+    public int _endYear = 2023;
 
     public void Display()
     {
         Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
+
 }
+
+    
+
